254a2b6bc78ee6ef7fff23dccf2f48c5a7c8dc79
alwaysad <causeimmad@hotmail.com>
2024-06-01 19:17:09 +0300

    code added [skip ci]

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import MainContent from './components/MainContent';
-import logo from './assets/images/logo.png';
+import logo from "./assets/images/logo.png";
+import MainContent from "./components/MainContent";
 
 function App() {
+  //new code
   return (
     <>
       <header>
